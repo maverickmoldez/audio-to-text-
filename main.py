79497d70b38f411afa066a9f5539e55ef38e1e6b
maverickmoldez <139948380+maverickmoldez@users.noreply.github.com>
2023-12-13 22:36:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import speech_recognition as sr
-
+import numpy as np
 
 recognizer = sr.Recognizer()
-
 
 with sr.Microphone() as source:
     print("Say something...")
     audio = recognizer.listen(source)
 
-  
     try:
+        
+        audio_signal = np.frombuffer(audio.frame_data, dtype=np.int16)
+
+        print("Audio Signal:", audio_signal)
+
         text = recognizer.recognize_google(audio)
         print("You said:", text)
     except sr.UnknownValueError:
